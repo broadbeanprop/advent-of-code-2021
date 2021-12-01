3890a0eb51557cd3692eb80e483c1bb3a4b4db6d
Jeffrey Boon <jeffrey.boon@rtl.nl>
2021-12-01 12:06:32 +0100

    Day 1

---
+++ Day1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+var input = File.ReadLines(@"input.txt").Select(int.Parse).ToList();
+
+Part1();
+Part2();
+
+void Part1()
+{
+    var increasedDepthCounter = 0;
+    int? previousDepth = null;
+
+    foreach (var depth in input)
+    {
+        if (depth > previousDepth)
+            increasedDepthCounter++;
+
+        previousDepth = depth;
+    }
+
+    Console.WriteLine($"The answer to part 1 is: {increasedDepthCounter}");
+}
+
+void Part2()
+{
+    var increasedDepthCounter = 0;
+    int? previousWindowResult = null;
+    var skip = 0;
+    
+    while (true)
+    {
+        var window = input.Skip(skip).Take(3).ToList();
+        
+        if (window.Count < 3)
+            break;
+
+        var currentWindowResult = window.Sum();
+        
+        if (currentWindowResult > previousWindowResult)
+            increasedDepthCounter++;
+
+        previousWindowResult = currentWindowResult;
+        skip++;
+    }
+    
+    Console.WriteLine($"The answer to part 2 is: {increasedDepthCounter}");
+}
